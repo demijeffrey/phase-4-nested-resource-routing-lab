d23cb47bdf5e7a65d0529c0e679081bc99e3a22e
Demaris Ashmore <demiashmore@yahoo.com>
2023-01-26 13:02:33 -0700

    commit tree 1959c06cdf12a72ba9d048fcb7ec55995ad8c350

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,41 @@
 class ItemsController < ApplicationController
 
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create!(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def record_not_found(error)
+    render json: { error: "#{error.model} not found" }, status: :not_found
+  end
+
+  def invalid_record(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
 class UsersController < ApplicationController
 
+  # def show
+  #   user = User.find_by(id: params[:id])
+  #   render json: user, include: :items
+  # end
+
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
   end
 
 end
+
+
+
+# def index
+#   if params[:dog_house_id]
+#     dog_house = DogHouse.find(params[:dog_house_id])
+#     reviews = dog_house.reviews
+#   else
+#     reviews = Review.all
+#   end
+#   render json: reviews, include: :dog_house
+# end
